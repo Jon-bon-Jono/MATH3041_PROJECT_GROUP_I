e5c2ebee96049a08650ed64c1a1c93820469a34e
Harrison Falconer <z5364172@ad.unsw.edu.au>
2022-07-21 12:21:45 +1000

    Added simple model.py file with docstring

---
+++ model.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def model(carbon_data):
+    '''
+    function name and input names may be changed to something more appriopriate.
+
+    This takes in a list of CO2 levels for each year over a decedal period of time. 
+    This data is then put into the chosen mathematical model to produce a list of 
+    tuples that contains the expected temperature for a given year given is CO2 levels.
+
+    Arguments:
+        carbon_data (list of tuples(?))             - Contains the CO2 levels for each year in the
+                                                      given decedal period
+        
+    Return value:
+        returns temp_data (list of tuples(?))
+    '''
+
+
+
+    # TODO
+
+
+    return temp_data
+    
+
